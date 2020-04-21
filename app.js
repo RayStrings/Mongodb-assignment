119100db5b60ddb738667ed5ba55a22841da0eac
Ray Addo <addoray40@gmail.com>
2020-04-21 13:26:40 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+
+//Root Route
+app.use('/', require ('./routes/'));
+
+
+// Start application on port 4800
+const port = 4800;
+app.listen(port, ()=>{
+    console.log(`Server has started on port ${port}`);
+});
